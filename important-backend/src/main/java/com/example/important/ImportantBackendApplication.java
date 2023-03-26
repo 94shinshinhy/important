171fb2665db58f2b012f48daee376f3530340bb5
Owner <94shinshinhy@gmail.com>
2023-03-26 16:03:51 +0900

    setting

---
+++ important-backend/src/main/java/com/example/important/ImportantBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.important;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImportantBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImportantBackendApplication.class, args);
+	}
+
+}
---
+++ important-backend/src/test/java/com/example/important/ImportantBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.important;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImportantBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
